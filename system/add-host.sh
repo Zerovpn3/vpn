736a84c37bc255e87eb77bdb549e2adc9e91a885
Zerovpn3 <134721008+Zerovpn3@users.noreply.github.com>
2023-05-27 12:52:11 +0700

    Update add-host.sh

--- system/add-host.sh
+++ system/add-host.sh
@@ -24,15 +24,15 @@ VALIDITY () {
 fi
 }
 IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-sleep 0.1
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
+#if [ $MYIP = $IZIN ]; then
+#echo -e "\e[32mPermission Accepted...\e[0m"
+#VALIDITY
+#sleep 0.1
+#else
+#echo -e "\e[31mPermission Denied!\e[0m";
+#echo -e "\e[31mPlease buy script first\e[0m"
+#exit 0
+#fi
 
 # // GMAIL && DOMAIN
 clear
