3711636f6af2ebe796ce91154b1dee6ccf717553
Zerovpn3 <134721008+Zerovpn3@users.noreply.github.com>
2023-05-27 10:33:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+apt update
+apt install -y bzip2 gzip coreutils screen curl
 #wget https://github.com/${GitUser}/
 GitUser="zerovpn3"
 # Color Validation
