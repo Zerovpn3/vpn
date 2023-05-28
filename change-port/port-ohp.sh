42887fa1f6bc0d78ca0475315c5f3edbf0bcfc9d
Zerovpn3 <134721008+Zerovpn3@users.noreply.github.com>
2023-05-29 02:01:23 +0700

    Update port-ohp.sh

--- change-port/port-ohp.sh
+++ change-port/port-ohp.sh
@@ -7,7 +7,7 @@ clear
 # Valid Script
 VALIDITY () {
     today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    Exp1=$(curl https://raw.githubusercontent.com/Zerovpn3/sc/main/listip | grep $MYIP | awk '{print $4}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
     else
@@ -16,15 +16,15 @@ VALIDITY () {
     exit 0
 fi
 }
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-#if [ $MYIP = $IZIN ]; then
-#echo -e "\e[32mPermission Accepted...\e[0m"
-#VALIDITY
-#else
-#echo -e "\e[31mPermission Denied!\e[0m";
-#echo -e "\e[31mPlease buy script first\e[0m"
-#exit 0
-#fi
+IZIN=$(curl https://raw.githubusercontent.com/Zerovpn3/sc/main/listip | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
 echo -e "\e[32mloading...\e[0m"
 clear
 ohpssh="$(cat ~/log-install.txt | grep -w "OHP SSH" | cut -d: -f2|sed 's/ //g')"
@@ -53,7 +53,7 @@ if [[ -z $cek ]]; then
 rm -f /etc/systemd/system/ohps.service
 cat > /etc/systemd/system/ohps.service <<END
 [Unit]
-Description=Direct Squid Proxy For open-ssh By PAKYAVPN 
+Description=Direct Squid Proxy For open-ssh By ARNOLD
 Documentation=https://t.me/anakjati567
 Wants=network.target
 After=network.target
