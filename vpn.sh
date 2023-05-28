b5db305e3c1939613a74c474eec6070b03fed2e3
Zerovpn3 <134721008+Zerovpn3@users.noreply.github.com>
2023-05-29 04:37:23 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/${GitUser}/hangtuah/main/vpn.zip
+wget https://raw.githubusercontent.com/${GitUser}/zerovpn3/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
